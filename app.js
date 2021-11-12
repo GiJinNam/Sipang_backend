e3b47934e9d4ec85c1e9f292ca54c2b6bc8c4c59
GiJinNam <87312828+GiJinNam@users.noreply.github.com>
2021-11-13 00:28:12 +0900

    heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import morgan from 'morgan'
+//import morgan from 'morgan'
 import hpp from 'hpp'
 import helmet from 'helmet'
 import { MONGO_URI } from './config/index.js'
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 app.use(hpp())
 app.use(helmet())
 app.use(cors({ origin, credentials: true }))
-app.use(morgan('dev'))
+//app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
