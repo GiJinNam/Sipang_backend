b64aa02478935ba85aa367bf82590a754fe6107c
GiJinNam <87312828+GiJinNam@users.noreply.github.com>
2021-11-15 11:11:01 +0900

    gym라우터 검색기능 추가

--- Router/gym.js
+++ Router/gym.js
@@ -2,7 +2,11 @@ import express from 'express'
 import Gym from '../models/Gym.js'
 
 const router = express.Router()
-
+/**
+ * POST /gym
+ * 체육관을 등록하는 API (나중에 어드민만 사용할 수 있도록 설정해야함)
+ *
+ */
 router.post('/', async (req, res) => {
 	try {
 		const { name, address, tel, coat, price } = req.body
@@ -24,13 +28,36 @@ router.post('/', async (req, res) => {
 	}
 })
 
+/**
+ * GET /gym
+ * 모든 체육관 내용 가져와주는 API
+ */
 router.get('/', async (req, res) => {
 	try {
 		const gym = await Gym.find()
-		return res.status(200).send(gym)
+		return res.status(200).json(gym)
 	} catch (error) {
 		console.error(error)
-		return res.status(500).send(error)
+		return res.status(500).json(error)
+	}
+})
+
+/**
+ * GET /gym/:id
+ * 검색 내용으로 체육관 목록 찾아주는 API
+ */
+router.get('/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		//name값에 받아온 id값이 포함되어 있는 데이터를 찾아서 gym에 넣음.
+		const gym = await Gym.find({ name: { $regex: id } })
+		if (gym.length === 0) {
+			return res.status(400).json({ message: '검색결과가 없습니다.' })
+		}
+		return res.status(200).json(gym)
+	} catch (error) {
+		console.error(error)
+		return res.status(500).json(error)
 	}
 })
 
--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-//import morgan from 'morgan'
+import morgan from 'morgan'
 import hpp from 'hpp'
 import helmet from 'helmet'
 import { MONGO_URI, COOKIE_SECRET } from './config/index'
 import gymRouter from './router/gym'
 import userRouter from './router/user'
-import passport, { Passport } from 'passport'
+import passport from 'passport'
 import session from 'express-session'
 import cookieParser from 'cookie-parser'
 import passportConfig from './passport'
@@ -15,23 +15,25 @@ import passportConfig from './passport'
 const app = express()
 const origin = 'http://localhost:3000'
 const PORT = process.env.PORT || 5000
+const sessionOption = {
+	saveUninitialized: true,
+	resave: false,
+	secret: COOKIE_SECRET,
+	rolling: true,
+	cookie: {
+		secure: false,
+		maxAge: process.env.DEBUG === '1' ? Infinity : 1000 * 60 * 30
+	}
+}
 
 app.use(hpp())
 app.use(helmet())
 app.use(cors({ origin, credentials: true }))
-//app.use(morgan('dev'))
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(
-	cookieParser(process.env.COOKIE_SECRET, { sameSite: 'none', secure: true })
-)
-app.use(
-	session({
-		saveUninitialized: false,
-		resave: false,
-		secret: COOKIE_SECRET
-	})
-)
+app.use(cookieParser(COOKIE_SECRET))
+app.use(session(sessionOption))
 app.use(passport.initialize())
 app.use(passport.session())
 
